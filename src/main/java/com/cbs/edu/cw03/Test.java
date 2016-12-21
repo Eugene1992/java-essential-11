4b1bcbfcf02359d72d156100ddbd95903646cd49
Eugene1992 <deyneko55@gmail.com>
2016-12-21 18:46:37 +0200

    Initial test branch.

--- src/main/java/com/cbs/edu/cw03/Test.java
+++ src/main/java/com/cbs/edu/cw03/Test.java
@@ -4,7 +4,5 @@ import com.cbs.edu.cw02.Employee;
 
 public class Test {
     public static void main(String[] args) {
-        Employee employee = new Employee();
-//        employee.name = "Iban";
     }
 }
