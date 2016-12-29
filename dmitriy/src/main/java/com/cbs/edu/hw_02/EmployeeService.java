bc830c1bc87cb03c7b967a4464de20e0d38f289c
Dmitry1397 <dyshik8@gmail.com>
2016-12-29 18:00:09 +0200

    #8 add exception

--- dmitriy/src/main/java/com/cbs/edu/hw_02/EmployeeService.java
+++ dmitriy/src/main/java/com/cbs/edu/hw_02/EmployeeService.java
@@ -2,6 +2,7 @@ package com.cbs.edu.hw_02;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by Дмитрий on 25.12.2016.
@@ -12,8 +13,8 @@ public final class EmployeeService {
     }
 
     public static List<Employee> getSalaryInterval(Department department, int min, int max) {
-
         List<Employee> employees = new ArrayList<>();
+        if(min < 0 || max < min || max < 0) throw new IllegalArgumentException("Value of 'min' is negative or 'max' > 'min'");
         for (Employee person : department.getEmployees()) {
             if (person.getSalary() <= max && min <= person.getSalary()) {
                 employees.add(person);
@@ -33,9 +34,14 @@ public final class EmployeeService {
         return employees;
     }
 
-    public static List<Employee> addBonus(Department department, double bonus) {
+    public static List<Employee> addBonus(Department department, double bonus) throws MyException{
 
         List<Employee> employees = new ArrayList<>();
+        try {
+            bonus = 0;
+        } catch (IllegalArgumentException e) {
+             throw new MyException("Bonus can not be 0");
+        }
         for (Employee person : department.getEmployees()) {
             double salary = person.getSalary() + bonus * person.getSalary();
             person.setSalary((int)salary);
